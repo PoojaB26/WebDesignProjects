f5d1860dbb5602113cab904d43399d4e7344e008
poojab26 <pbhaumik26@gmail.com>
2016-12-22 01:04:14 +0530

    gallery & modal using javascript

--- gallery-basics/js/gallery-functions.js
+++ gallery-basics/js/gallery-functions.js
@@ -2,12 +2,13 @@
  * Created by pblead26 on 12-Dec-16.
  */
 
-function displayBig() {
-      var modal = document.getElementById('myModal');
-     var modalImg = document.getElementById("img01");
-     modal.style.display = "block";
-     modalImg.src = document.getElementById(this).src;
-     }
-   // document.getElementById('heading').innerHTML = document.getElementsByClassName('preview')[this.index].src;
+
+function displayBig(previewPic) {
+    modal = document.getElementById('modal_image');
+
+    modal.style.display = "block";
+    document.getElementById('imageBig').style.backgroundImage = "url('" + previewPic.getAttribute("src")+"')";
+}
+
 
 
