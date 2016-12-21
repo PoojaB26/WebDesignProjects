0f084b0abfba04c28c8bf20caabd3bb4bb918672
poojab26 <pbhaumik26@gmail.com>
2016-12-21 18:07:57 +0530

    update

--- gallery-basics/js/gallery-functions.js
+++ gallery-basics/js/gallery-functions.js
@@ -1,3 +1,13 @@
 /**
  * Created by pblead26 on 12-Dec-16.
  */
+
+function displayBig() {
+      var modal = document.getElementById('myModal');
+     var modalImg = document.getElementById("img01");
+     modal.style.display = "block";
+     modalImg.src = document.getElementById(this).src;
+     }
+   // document.getElementById('heading').innerHTML = document.getElementsByClassName('preview')[this.index].src;
+
+
