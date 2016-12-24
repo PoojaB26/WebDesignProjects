a567961f2566617d64d84a3cb728107cf6d8a174
poojab26 <pbhaumik26@gmail.com>
2016-12-25 01:18:27 +0530

    New project - CSS Animation I

---
+++ animations-plusToEqual/js/function.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pblead26 on 25-Dec-16.
+ */
+document.querySelector( "#nav-toggle" )
+    .addEventListener( "click", function() {
+        this.classList.toggle( "active" );
+    });
